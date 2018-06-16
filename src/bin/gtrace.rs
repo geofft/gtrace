6379310edd243e88920d8ee368beedfde9be751d
anatol <anatol1988@gmail.com>
2018-06-16 15:34:57 +0300

    Add option of attaching to existing process

--- src/bin/gtrace.rs
+++ src/bin/gtrace.rs
@@ -2,29 +2,74 @@ extern crate libc;
 extern crate gtrace;
 extern crate nix;
 
+use std::env::Args;
 use std::os::unix::process::CommandExt;
+use std::process;
 use std::process::Command;
 use nix::sys::wait::waitpid;
 
 use gtrace::TraceEvent;
 
-fn main() {
-    let mut args = std::env::args_os().skip(1);
-    let mut cmd = Command::new(args.next().expect("usage: gtrace cmd [args...]"));
-    for arg in args {
-        cmd.arg(arg);
+enum App {
+    Run(String, Vec<String>),
+    Attach(libc::pid_t),
+}
+
+fn parse_args(args: &Vec<String>) -> Option<App> {
+    let pos = args.iter().position(|ref x| x == &"-p");
+
+    if let Some(pos) = pos {
+        println!("I'm here");
+        let pid = args.iter().nth(pos + 1).expect("Can't find PID");
+        let pid = pid.parse::<libc::pid_t>().expect("Can't parse PID");
+        return Some(App::Attach(pid));
+    } else {
+        let name = args.first();
+        println!("{:?}", name);
+        let args = args.iter().skip(1).cloned().collect();
+        println!("{:?}", args);
+        return name.map(|x| App::Run(x.to_string(), args));
     }
-    cmd.before_exec(gtrace::traceme);
-    let child = cmd.spawn().expect("child process failed");
-    let pid = nix::unistd::Pid::from_raw(child.id() as libc::pid_t);
-    let mut tracee = gtrace::Tracee::new(pid);
+}
+
+fn get_tracee(app: App) -> (Option<process::Child>, nix::unistd::Pid, gtrace::Tracee) {
+    match app {
+        App::Run(name, args) => {
+            let mut cmd = Command::new(name);
+            for arg in args {
+                cmd.arg(arg);
+            }
+            cmd.before_exec(gtrace::traceme);
+            let child = cmd.spawn().expect("child process failed");
+            let pid = nix::unistd::Pid::from_raw(child.id() as libc::pid_t);
+            return (Some(child), pid, gtrace::Tracee::new(pid));
+        }
+        App::Attach(pid) => {
+            let pid = nix::unistd::Pid::from_raw(pid);
+            println!("{:?}", pid);
+            (None, pid, gtrace::Tracee::new(pid))
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    let app = parse_args(&args);
+
+    if let None = app {
+        println!("gtrace: must have PROG [ARGS] or -p PID");
+        process::exit(0)
+    }
+
+    let app = app.unwrap();
+    let (_, pid, mut tracee) = get_tracee(app);
     loop {
         match tracee.step(waitpid(pid, None).unwrap()) {
             TraceEvent::SysEnter => print!("{}", gtrace::decode::decode(&mut tracee).unwrap()),
             TraceEvent::SysExit => println!(" = {}", tracee.get_return().unwrap()),
             TraceEvent::Signal(sig) => println!("** signal {:?}", sig),
             TraceEvent::Exit(_) => { break; }
-        }
+            }
         tracee.run().unwrap();
     }
     //child.wait().unwrap();
